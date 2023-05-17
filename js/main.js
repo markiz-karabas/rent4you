b3c27eb983d108ad9bde09ad9c2ef47026cae4da
markiz-karabas <vsevolodova@inbox.ru>
2023-05-17 21:30:54 +0300

    refactor: fix url

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.slider').slick({
-        nextArrow: '<button class="slick-arrow slick-next"><img src="/assets/png/next.png" alt="arrow"></button>',
-        prevArrow: '<button class="slick-arrow slick-prev"><img src="/assets/png/prev.png" alt="arrow"></button>'
+        nextArrow: '<button class="slick-arrow slick-next"><img src="./assets/png/next.png" alt="arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="./assets/png/prev.png" alt="arrow"></button>'
     });
 });
     
